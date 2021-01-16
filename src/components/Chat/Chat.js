fc24ffa662dfec135f7e92538deedab5a0c0d15d
jumpybuns <70243662+jumpybuns@users.noreply.github.com>
2021-01-15 16:31:28 -0800

    add useEffect and Chat function to Chat.js (#4)

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+import PropTypes from 'prop-types';
 
-export default function Chat() {
+import styles from './Chat.css';
+
+let socket;
+
+function Chat({ location }) {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+  const ENDPOINT = 'localhost:5000';
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit('join', { name, room });    
+  }, [ENDPOINT, location.search]);
+
   return (
-    <div>
+    <h1>
       Chat
-    </div>
+    </h1>
   );
 }
+
+Chat.propTypes = { 
+  location: PropTypes.object.isRequired
+};
+
+export default Chat;
