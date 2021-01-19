7183945151ba6918e0cbb65a8da2e4791419a823
davidabutler92 <davidabutler92@yahoo.com>
2021-01-18 22:30:31 -0800

    add scrollToTop function to RoomList component

--- src/components/RoomContainer/RoomContainer.js
+++ src/components/RoomContainer/RoomContainer.js
@@ -7,18 +7,6 @@ import Header from '../Header/Header';
 export default function RoomContainer({ user, socket, handleLogout }) {
   const [rooms, setRooms] = useState([]);
 
-  const roomData = [
-    { name: 'super loooooong name', id: '1' },
-    { name: 'room two', id: '2' },
-    { name: 'room three', id: '3' },
-    { name: 'super loooooong name', id: '4' },
-    { name: 'room five', id: '5' },
-    { name: 'room six', id: '6' },
-    { name: 'super seven', id: '7' },
-    { name: 'room eight', id: '8' },
-    { name: 'room nine', id: '9' },
-  ];
-
   useEffect(() => {
     if (socket) {
       socket.emit('GET_ROOMS', user?.id);
@@ -33,7 +21,7 @@ export default function RoomContainer({ user, socket, handleLogout }) {
       <Header user={user} handleLogout={handleLogout} />
       <section className={styles.container}>
         <div className={styles.roomListContainer}>
-          <RoomList rooms={roomData} />
+          <RoomList rooms={rooms} />
         </div>
         <div className={styles.roomFormContainer}>
           <RoomForm user={user} socket={socket} />
